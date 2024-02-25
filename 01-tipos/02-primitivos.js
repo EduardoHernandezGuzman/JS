08ff74122061090113737423ca5b0a2d6017bb29
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-25 11:40:29 +0100

    Creado 02-primitivos.js

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,20 @@
+//TIPOS DE DATOS = primitivos y de referencia
+// Primitivos : Number, String, Boolean, Undefined, Null
+// De referencia: Array, Object, Funtions, Class
+
+//Number
+let numero = 1;
+
+//String
+let textp = "Hola mundo";
+
+//Boolean
+let verdadero = true;
+let falso = false;
+
+//Undefined
+let noDefinido;
+let unDef = undefined;
+
+//Null
+let nulo = null;
