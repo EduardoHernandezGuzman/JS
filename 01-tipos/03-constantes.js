056d99f94adce458eb94c137690005b96e12647d
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-25 11:48:54 +0100

    Creado 03-constantes.js

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,13 @@
+//Definimos la variable texto
+let texto = "Hola";
+//Cambiamos el valor de la variable texto
+texto = "Hola mundo";
+
+console.log(texto);
+
+
+//Si usamos constantes en lugar de variables, no podemos cambiar su valor 
+//a posteriori. Nos arrojaría un error.
+const estoEsUnaConstante = 3.1416;
+
+console.log(estoEsUnaConstante);
