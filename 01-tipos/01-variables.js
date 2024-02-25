fa86124749ac2e2a5d7685eb81ee41f7b765c14c
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-25 11:26:28 +0100

    Actualizado 01-variables.js

--- 01-tipos/01-variables.js
+++ 01-tipos/01-variables.js
@@ -1,3 +1,24 @@
-let nombre = "Hola mundo";
+//VARIABLE = contenedor de información al que se le asigna un nombre único.
 
-console.log(nombre)
+//DIFERENTES FORMAS DE ESCRITURA : UpperCamelCase , camelCase , snake_case
+//JS es case sensitive => estás 3 son variables diferentes.
+let NombreCompleto = ("Alfredo Landa");     //UpperCamelCase
+let nombreCompleto = ("Rocío Durcal");      //camelCase
+let nombre_completo = ("Isabel Pantoja");   //snake_case
+
+console.log(NombreCompleto);
+console.log(nombreCompleto);
+console.log(nombre_completo);
+
+
+
+//Aquí definimos la variable nombre
+let nombre = "Silvester";
+
+console.log(nombre);
+
+//También podemos definir así la variable
+let apellido;
+apellido = "Stallone"
+
+console.log(apellido);
