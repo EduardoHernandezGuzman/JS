7724e4616ef6593b2fc6cc5f0abc081a3985751c
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-25 11:59:00 +0100

    Creado 04-tipado-dinamico.js

---
+++ 01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,28 @@
+//TIPADO ESTÁTICO = En el tipado estático, los tipos de datos de todas las variables 
+                    //deben ser declarados explícitamente en tiempo de compilación.
+
+                    // int numero = 5;
+                    // String nombre = "Juan";
+
+                    // utilizan tipado estático son Java, C, C++, TypeScript,...
+
+//TIPADO DINÁMICO = los tipos de datos de las variables se deducen 
+                    //automáticamente en tiempo de ejecución. 
+
+                    // let numero = 5;
+                    // let nombre = "Juan";
+
+                    // es el que utilizamos en JavaScript
+
+let numero = 23;
+let texto = "texto de prueba";
+let verdadero = true;
+let undef;
+let nulo = null;
+
+//typeof nos dice el tipo de dato
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nulo);
