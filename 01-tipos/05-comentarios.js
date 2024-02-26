e5e2199e3123734ba871581c6f48acc39c654a67
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-26 20:11:04 +0100

    Creado 05-comentarios.js

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,26 @@
+//Este es un comentario
+let numero = 42;
+
+console.log(numero); // Este es otro comnentario
+
+
+
+/**
+ * 
+ *  Con   / * *  en VisualStudioCode 
+ *  podemos hacer un comentario
+ *  de varias líneas
+ * 
+ */
+
+
+
+
+
+/*
+
+Esta es la forma oficial de JS de 
+hacer comentarios de varias
+líneas
+
+*/ 
