1977702dfca8352c846d54872d8ae8c5ae83c225
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-27 19:25:52 +0100

    Creado 08-funciones.js

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,19 @@
+// FUNCIÓN =  bloque de código que realiza 
+              //una tarea específica cuando es invocado o llamado. 
+function saludar() {
+    console.log("Hola, buenas tardes");
+};
+
+saludar();
+
+
+//
+
+
+function suma() {
+    return 2 + 2;
+};
+
+let resultadoSuma = suma();
+console.log(resultadoSuma);
+console.log(suma());
