16f3e8c82a673564ae2c65a9b5ae5fe2190f10be
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-27 20:46:08 +0100

    Creado02-operadores/01-aritmeticos.js

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,30 @@
+let a = 5;
+let b = 7;
+
+console.log(a + b, "adición"); //adición
+console.log(a - b, "resta"); //resta
+console.log(a * b, "multiplicación"); //multiplicación
+console.log(a / b, "división"); //división
+console.log(a % b, "módulo"); //módulo (calcula el resto 
+                              //de la división de a entre b)
+console.log(a ** b, "potencia"); //potencia
+
+
+//incrementar
+console.log(++a) //5 + 1 = 6
+console.log(++a) //6 + 1 = 7
+console.log(++a) //7 + 1 = 8
+    //aquí primero imprime y luego con otro 
+    //console.log se imprime ese incremento
+console.log(a++) 
+console.log(a) //9
+
+
+//"decrementar"
+console.log(--a) //9 - 1 = 8
+console.log(--a) //8 - 1 = 7
+console.log(--a) //7 - 1 = 6
+    //aquí primero imprime y luego con otro 
+    //console.log se imprime ese 
+console.log(a--)
+console.log(a) //5
