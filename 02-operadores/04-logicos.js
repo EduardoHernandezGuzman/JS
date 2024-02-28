b670ebe146c8b1c8a6692b11e1c7006927c91ecf
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-28 10:30:53 +0100

    Creado 04-logicos.js

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,25 @@
+// AND &&
+
+//si los dos son true, devuelve true
+console.log(true && true); 
+//si uno de los dos es false, devuelve false
+console.log(false && true); 
+
+let mayorDeEdad = false;
+let suscrito = true;
+console.log("Operador AND", mayorDeEdad && suscrito);
+
+
+
+// OR ||
+
+//devuelve true si al menos uno de los dos es true
+//sólo devuelve false si los dos son false
+console.log("Operador OR", mayorDeEdad || suscrito);
+
+
+
+// NOT !
+
+//el operador NOT me va a invertir el valor
+console.log("Operador NOT", !mayorDeEdad);
