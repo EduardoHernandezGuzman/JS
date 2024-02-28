d7ac2cece7e11ba32c691ec88a1c4a9f690898ed
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-28 11:06:02 +0100

    Creado 06-bitwise.js

---
+++ 02-operadores/06-bitwise.js
@@ -0,0 +1,35 @@
+// NO LO VAMOS A USAR . SE USA MUY MUY MUY POCO.
+
+
+// Decimal: 0,1,2,3,4,5,6,7,8,9
+// Binario: 0,1
+// bit: 0 1
+// Byte: 8 bits
+// Byte: 00000000 -> 0
+// Byte: 00000001 -> 1
+// Byte: 00000010 -> 2
+// Byte: 00000011 -> 3
+// Byte: 00000100 -> 4
+// Byte: 00000101 -> 5
+// Byte: 00000110 -> 6
+
+
+// La operación bitwise se refiere a la manipulación de bits individuales en números binarios. Los operadores bitwise actúan directamente sobre los bits de los números en su representación binaria. Estos operadores son útiles cuando necesitas realizar operaciones a nivel de bit, como manipulación de datos compactos, criptografía, optimización de código y otras aplicaciones donde la eficiencia y el control detallado sobre los bits son importantes.
+
+// Algunos de los operadores bitwise comunes en JavaScript son:
+
+// AND bitwise (&): Realiza una operación AND lógica a nivel de bit en dos números. Devuelve un 1 para cada posición de bit donde ambos operandos tengan un 1.
+
+// OR bitwise (|): Realiza una operación OR lógica a nivel de bit en dos números. Devuelve un 1 para cada posición de bit donde al menos uno de los operandos tenga un 1.
+
+// XOR bitwise (^): Realiza una operación XOR lógica a nivel de bit en dos números. Devuelve un 1 para cada posición de bit donde exactamente uno de los operandos tenga un 1.
+
+// NOT bitwise (~): Realiza una operación NOT lógica a nivel de bit en un solo número. Invierte cada bit del número, convirtiendo cada 0 en un 1 y cada 1 en un 0.
+
+// Desplazamiento a la izquierda (<<): Desplaza los bits de un número hacia la izquierda en una cantidad específica de posiciones, llenando los bits vacíos en la derecha con ceros.
+
+// Desplazamiento a la derecha con signo (>>): Desplaza los bits de un número hacia la derecha en una cantidad específica de posiciones, llenando los bits vacíos en la izquierda con el bit de signo original.
+
+// Desplazamiento a la derecha sin signo (>>>): Desplaza los bits de un número hacia la derecha en una cantidad específica de posiciones, llenando los bits vacíos en la izquierda con ceros.
+
+// Estos operadores pueden ser útiles en situaciones donde necesitas manipular bits individuales o realizar operaciones a nivel de bit en datos binarios.
