8ef74db53ca046fec36788ff5e5fa29f196a0eed
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-28 10:06:42 +0100

    Creado 02-asignacion.js

---
+++ 02-operadores/02-asignacion.js
@@ -0,0 +1,19 @@
+let a = 5;
+a = a +5;
+console.log(a);
+
+
+//otra forma de hacerlo es usando el
+//operador de asignación de suma
+a += 5;
+console.log(a);
+
+
+let b = 10;
+b += 10;
+b -= 10;
+b *= 10;
+b /= 10;
+b %= 10;
+b **= 10;
+console.log(b);
