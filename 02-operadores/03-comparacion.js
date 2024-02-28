aa879608c53e8dd19e830951d1a8f1ac1193cf5f
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-28 10:18:43 +0100

    Creado 03-comparacion.js

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,26 @@
+//OPERADORES RELACIONALES
+let a = 10;
+//cuando usemos operadores de comparación,
+//nos va a devolver un boolean
+console.log(a > 5);
+
+let b = 5;
+console.log(b > 5);
+console.log(b >= 5);
+console.log(b < 5);
+console.log(b <= 5);
+
+
+//OPERADORES DE IGUALDAD
+let c = 10;
+console.log(c == 10);
+console.log(c != 10);
+console.log(c == "10"); //devuelve true porque estamos preguntando
+                        //por la igualdad del valor
+console.log(c ==="10"); //devuelve false porque estamos preguntando 
+                        //por la igualdad del valor y del tipo del dato
+console.log(c !== "10");
+
+
+// Hoy en día se usan === y !==
+// == y != ya están en deshuso
