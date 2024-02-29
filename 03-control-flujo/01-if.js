fd75a501d0b1ad23f819e1ce63c783e28bbac247
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-02-29 11:11:56 +0100

    Creado 01-if.js

---
+++ 03-control-flujo/01-if.js
@@ -0,0 +1,22 @@
+//Para ejecutar una porción de código unicaménte
+//cuando se cumpla una condición
+
+        //       if (condición) {
+        //           expresión
+        //       };
+
+
+let edad = 25;
+
+if (edad > 18) {
+    console.log("Usuario mayor de edad")
+};
+
+
+
+
+let edad2 = 14;
+
+if (edad2 < 18) {
+    console.log("Usuario menor de edad")
+};
