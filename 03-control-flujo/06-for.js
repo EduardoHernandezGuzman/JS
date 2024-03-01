250b9858582fa28460c1c06022c0407347acd960
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-03-01 19:05:12 +0100

    Creado 06-for.js

---
+++ 03-control-flujo/06-for.js
@@ -0,0 +1,12 @@
+//For al igual que While nos sirve para hacer un loop, pero
+//a diferencia de while, for tiene todo el código en una línea
+
+        //      for (inicio; condición; incremento) {
+        //          Bloque de código a ejecutar
+        //      }
+    
+for (let i = 0 ; i < 10 ; i++) {
+    if (i % 2 == 0){ 
+        console.log("Número par" , i);
+    }
+};
