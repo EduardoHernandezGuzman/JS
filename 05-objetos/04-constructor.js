c989ed92c27ca096c58dc5043f05d95bc3b7a752
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-04-04 13:48:24 +0200

    Modificado 04-constructor.js

--- 05-objetos/04-constructor.js
+++ 05-objetos/04-constructor.js
@@ -14,3 +14,20 @@ function Usuario () {
 let usuario01 = new Usuario();
 
 console.log(usuario01);
+
+
+// Creamos una función creadora Alumno
+function Alumno (nombre, edad, curso,) {
+    this.name = nombre;
+    this.age = edad;
+    this.course = curso;
+};
+
+// Creamos 3 instancias de alumno
+let david = new Alumno ("David", 19, "2024");
+let roberto = new Alumno ("Roberto", 29, "2024");
+let manuel = new Alumno ("Manuel", 42, "2024");
+
+console.log(david);
+console.log(roberto);
+console.log(manuel);
