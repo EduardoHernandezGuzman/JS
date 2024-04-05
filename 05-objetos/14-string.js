70acffd6212c4390e2ceac939d1ba19d1a1f411c
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-04-05 14:25:23 +0200

    Creado 14-string.js

---
+++ 05-objetos/14-string.js
@@ -0,0 +1,17 @@
+const saludo = "Hola Mundo";
+
+console.log(
+    saludo.length,  // Longitud del string
+    saludo.indexOf("Mu"),    // Devuelve el índice donde se encuentra Mu
+    saludo.indexOf("xz"),   // Cuando no lo encuentra nos devuelve -1
+    saludo.includes("Mundo"),    // Nos verifica con true o false si lo incluye
+    saludo.replace("Mundo", "Clase"),   // Nos reemplaza el primer argumento por el segundo
+    saludo.toLowerCase(),   // Pasa todo el string a minúsculas
+    saludo.toLocaleUpperCase(), // Pasa todo el string a mayúsculas
+    saludo.substring(0, 7), // Inicia desde el indice 0 y acaba en el índice 7
+);
+
+const espacios = ".     Hola      Mundo     ";
+console.log(
+    espacios.trim() // Nos va a quitar los espacios a la izquierda y a la derecha
+); 
