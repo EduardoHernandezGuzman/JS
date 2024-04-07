9cfb9389bd22cff2f603a2f45ad33614c803ccbf
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-04-07 21:05:27 +0200

    Creado 15-escape.js

---
+++ 05-objetos/15-escape.js
@@ -0,0 +1,16 @@
+let saludo = "Hola este es un texto de prueba";
+console.log(saludo);
+
+// Hacer esto me devolvería un error
+// let saludo2 = "Hola este es un "texto" de prueba"; 
+// console.log(saludo2);
+
+// Para solucionar esto, tenemos los caracteres de escape.
+//      \n  nueva línea
+//      \t  tabulación
+//      \'  comilla simple
+//      \"  comilla doble
+//      \\  blackslash
+
+let saludo3 = "Hola \t este es \n un \"texto\" de prueba"; 
+console.log(saludo3);
