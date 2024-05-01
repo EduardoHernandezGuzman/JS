43261a4386fd0e90084ffa9adf8e1a4d9baca442
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-05-01 12:22:52 +0200

    Creado 02-let-var-const-fn.js

---
+++ 09-funciones/02-let-var-const-fn.js
@@ -0,0 +1,16 @@
+console.log(saludo);
+console.log(vieja);
+console.log(hola);
+console.log(adios);
+
+
+
+
+
+
+function saludo () {};
+let hola = "hola";
+const adios = "adios";
+var vieja = "ya no se usa var";
+
+
