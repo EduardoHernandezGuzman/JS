912bb0643d8dfcba08ec368810b653d792ea16a1
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-05-01 12:38:07 +0200

    Modificado 02-let-var-const-fn.js

--- 09-funciones/02-let-var-const-fn.js
+++ 09-funciones/02-let-var-const-fn.js
@@ -1,16 +1,12 @@
 console.log(saludo);
-console.log(vieja);
+console.log(vieja);  // var nos va a dar un problema de hoisting
 console.log(hola);
 console.log(adios);
-
-
-
-
-
 
 function saludo () {};
 let hola = "hola";
 const adios = "adios";
 var vieja = "ya no se usa var";
 
-
+// Para evitar problemas de hoisting, siempre 
+// utilizar let y const
