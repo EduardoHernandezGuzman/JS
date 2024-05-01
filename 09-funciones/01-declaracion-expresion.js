dc5af76a067d57c3faaecbbab1623f458c70a4a1
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-05-01 12:11:44 +0200

    Creado 01-declaracion-expresion.js

---
+++ 09-funciones/01-declaracion-expresion.js
@@ -0,0 +1,27 @@
+// Declaración de funciones. Lo hacemos con la 
+// palabra reservada function
+
+// Esto es una función nombrada (named function)
+function suma () {
+    console.log("Sumando ...");
+};
+
+// Función anónima (anonymous function)
+// function () {
+//     console.log("Función anónima ...");
+// };
+
+// Expresión de función (function expression)
+// Expresión de función anónima
+let resta = function () {
+    console.log("Restando ...");
+};
+
+// Función de expresión nombrada (named function expression)
+let multiplica = function multi () {
+    console.log("Multiplicando ...");
+};
+
+const divide = () => {
+    console.log("Dividiendo ...");
+};
